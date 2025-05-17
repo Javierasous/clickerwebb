eb34e7541fb8bb26b7e7b262654d3fec092802a8
Javierasous <javierale9988@gmail.com>
2025-05-17 16:21:09 -0400

    Build

---
+++ UnityProject/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,51 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void MenuManager_ClickObject_mC9BE0047943156565DEB7BE99F7AACB0A00FA000 (void);
+extern void MenuManager_PowerUp_m97FF665F419B1B56DCEC60E571DDC4118B7F74E7 (void);
+extern void MenuManager__ctor_m07A22DFDD90E3164393F8BDE06DAEF5AFA786CF2 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	MenuManager_ClickObject_mC9BE0047943156565DEB7BE99F7AACB0A00FA000,
+	MenuManager_PowerUp_m97FF665F419B1B56DCEC60E571DDC4118B7F74E7,
+	MenuManager__ctor_m07A22DFDD90E3164393F8BDE06DAEF5AFA786CF2,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	6273,
+	6273,
+	6273,
+	8661,
+	6273,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
